24e8b17991299b04cf0937604e4969dbfab9e1f5
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-05-05 14:50:06 +0300

    Create Payment Models

---
+++ lib/features/checkout/data/models/payment_intent_input_model.dart
@@ -0,0 +1,6 @@
+class PaymentIntentInputModel {
+  final String amount;
+  final String currency;
+
+  const PaymentIntentInputModel({required this.amount, required this.currency});
+}
---
+++ lib/features/checkout/data/models/payment_intent_model/AutomaticPaymentMethods.dart
@@ -0,0 +1,20 @@
+/// enabled : true
+
+class AutomaticPaymentMethods {
+  AutomaticPaymentMethods({
+      this.enabled,});
+
+  AutomaticPaymentMethods.fromJson(dynamic json) {
+    enabled = json['enabled'];
+  }
+  bool? enabled;
+AutomaticPaymentMethods copyWith({  bool? enabled,
+}) => AutomaticPaymentMethods(  enabled: enabled ?? this.enabled,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['enabled'] = enabled;
+    return map;
+  }
+
+}
