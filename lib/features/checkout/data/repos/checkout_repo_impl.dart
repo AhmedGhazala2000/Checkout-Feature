7e59353c77ce67c244ca54aa224cfa0f8f85b055
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-05-05 17:48:14 +0300

    Create checkout repos

---
+++ lib/features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:checkout_app/core/errors/failures.dart';
+import 'package:checkout_app/core/utils/stripe_services.dart';
+
+import 'package:checkout_app/features/checkout/data/models/payment_intent_input_model.dart';
+
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import 'checkout_repo.dart';
+
+class CheckoutRepoImpl extends CheckoutRepo {
+  final StripeServices stripeServices;
+
+  CheckoutRepoImpl({required this.stripeServices});
+
+  @override
+  Future<Either<Failures, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    try {
+      await stripeServices.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel,
+      );
+      return right(null);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioException(dioException: e));
+    } catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
